b5d3fc54a4c979c194288abd446e31f55bebd083
Juan Ramón Torres Guzmán <USIS054021@ugb.edu.sv>
2023-11-28 10:37:05 -0600

    kkkkkkkkkkkk

---
+++ ayuda.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SistemaFarmacia
+{
+    partial class ayuda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bup = new System.Windows.Forms.Button();
+            this.texto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bup
+            // 
+            this.bup.Location = new System.Drawing.Point(503, 331);
+            this.bup.Name = "bup";
+            this.bup.Size = new System.Drawing.Size(85, 37);
+            this.bup.TabIndex = 3;
+            this.bup.Text = "button1";
+            this.bup.UseVisualStyleBackColor = true;
+            // 
+            // texto
+            // 
+            this.texto.Enabled = false;
+            this.texto.Location = new System.Drawing.Point(72, 33);
+            this.texto.Multiline = true;
+            this.texto.Name = "texto";
+            this.texto.Size = new System.Drawing.Size(536, 287);
+            this.texto.TabIndex = 2;
+            this.texto.Text = " Se le otorgara ayuda";
+            // 
+            // ayuda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 390);
+            this.Controls.Add(this.bup);
+            this.Controls.Add(this.texto);
+            this.Name = "ayuda";
+            this.Text = "ayuda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bup;
+        private System.Windows.Forms.TextBox texto;
+    }
+}
---
+++ ayuda.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaFarmacia
+{
+    public partial class ayuda : Form
+    {
+        public ayuda()
+        {
+            InitializeComponent();
+        }
+        private void ayuda_Load(object sender, EventArgs e)
+        {
+
+
+            StreamReader Archivo = new StreamReader("c:\\ayuda.txt");
+
+
+            string Linea = "";
+
+
+            ArrayList Contenido = new ArrayList();
+
+
+            while (Linea != null)
+            {
+                Linea = Archivo.ReadLine();
+                if (Linea != null)
+                    Contenido.Add(Linea);
+                texto.Text = Linea;
+            }
+            Archivo.Close();
+
+
+            texto.Text = "";
+
+
+            foreach (string Linea_mostrar in Contenido)
+            {
+
+                texto.Text = texto.Text + Linea_mostrar + "Strings.Chr(13)";
+            }
+
+        }
+    }
+}
